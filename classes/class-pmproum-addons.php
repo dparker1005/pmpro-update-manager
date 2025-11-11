85f29ea83539ab602955d4dcd10186f419ef7058
David Parker <dlparker1005@gmail.com>
2025-11-11 14:56:41 -0500

    Fix deprecation warning

--- classes/class-pmproum-addons.php
+++ classes/class-pmproum-addons.php
@@ -726,7 +726,7 @@ class PMProUM_AddOns {
 	 */
 	private function get_upgrader_skin() {
 		// Use the core automatic skin to avoid direct output.
-		require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader-skins.php';
+		require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
 		$args = array(
 			'skip_header' => true,
 			'url'         => admin_url( 'plugins.php' ),
